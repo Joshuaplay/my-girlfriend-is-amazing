3a1faee2255c02aade667e20f8c12ad2c90f8dd4
Joshua Simons <mrjoshsimons@gmail.com>
2026-07-20 18:38:18 +1200

    Fix password check

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-jsconst correctPassword = "6thapril2025";
+const correctPassword = '06042025'
 
 function checkPassword() {
-  const input = document.getElementById("passwordInput").value;
-  const cleaned = input.trim().toLowerCase().replace(/\s+/g, "");
+  const input = document.getElementById('passwordInput').value
+  const cleaned = input
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '')
 
   if (cleaned === correctPassword) {
-    document.getElementById("gate").style.display = "none";
-    document.getElementById("mainContent").style.display = "block";
+    document.getElementById('gate').style.display = 'none'
+    document.getElementById('mainContent').style.display = 'block'
   } else {
-    document.getElementById("errorMsg").style.display = "block";
+    document.getElementById('errorMsg').style.display = 'block'
   }
 }
